5846557485e6501e1ce570cff0c021dcdcb2d41e
Yurii Antonchyk <xsat.php@gmail.com>
2024-07-26 13:59:10 +0300

    Add inventory is full detection

--- osrs_miner.py
+++ osrs_miner.py
@@ -1,3 +1,4 @@
+from cv2 import imread, IMREAD_COLOR
 from mouse import LEFT, RIGHT
 from numpy import ndarray
 
@@ -7,6 +8,8 @@ from osrs.game_status import GameStatus
 from osrs.character import Character
 
 
+UNDEFINED: int = 0
+
 MINING: int = 1
 
 MOVING_TO_BANK: int = 2
@@ -14,6 +17,11 @@ MOVING_TO_MINING_SITE: int = 3
 
 
 def osrs_miner() -> None:
+    status: int = UNDEFINED
+
+    inventory_is_too_ful_image: ndarray = imread('miner/inventory_is_too_full.png', IMREAD_COLOR)
+    clay_rocks_image: ndarray = imread('miner/clay_rocks.png', IMREAD_COLOR)
+
     game_status: GameStatus = GameStatus()
     character: Character = Character()
 
@@ -23,6 +31,11 @@ def osrs_miner() -> None:
         character.check_move()
         
         if game_status.is_opened():
+            if is_found(inventory_is_too_ful_image, screenshot):
+                print("Inventory is too full")
+            else:
+                print("Inventory is not full")
+            
             if character.is_moving():
                 print("Is moving")
             else:
