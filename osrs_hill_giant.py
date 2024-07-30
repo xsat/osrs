9c87bfc17f8b623389c415f0996c0a9478488378
Yurii Antonchyk <xsat.php@gmail.com>
2024-07-30 19:40:19 +0300

    PoC giants automation

--- osrs_hill_giant.py
+++ osrs_hill_giant.py
@@ -21,10 +21,19 @@ def _find_and_click_on_npc(image: ndarray, screenshot: ndarray, button: str) -> 
     return False
 
 
+def _find(image: ndarray, screenshot: ndarray) -> bool:
+    found_point: Point | None = find_point(image, screenshot)
+    if found_point != None:
+        return True
+
+    return False
+
+
 def osrs_hill_giant() -> None:
     game_status: GameStatus = GameStatus()
     character: Character = Character()
 
+    giant_health_image: ndarray = imread('hill_giant/giant_health.png', IMREAD_COLOR)
     giant_name_image: ndarray = imread('hill_giant/giant_name.png', IMREAD_COLOR)
 
     while game_status.is_runing():
@@ -35,7 +44,7 @@ def osrs_hill_giant() -> None:
         print("Is moving: ", character.is_moving())
         
         if game_status.is_opened() and not character.is_moving():
-            if _find_and_click_on_npc(giant_name_image, screenshot, RIGHT):
+            if not _find(giant_health_image, screenshot) and _find_and_click_on_npc(giant_name_image, screenshot, LEFT):
                 print("Hill Giant is found")
 
                 character.make_moving()
